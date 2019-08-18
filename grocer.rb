82d9fc0fe9cc7030a08cb76b3104aeb56fbdb987
Mykle jones <mykle.j91@gmail.com>
2019-08-18 23:24:46 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -19,7 +19,7 @@ def apply_coupons(cart, coupons)
   coupons.each do |coupon|
   #binding.pry 
   if cart.keys.include? coupon[:item]
-    if cart[coupon[:item]]
+    if cart[coupon[:item]][:count]
   
    ["#{item} W/COUPON"] = {count: coupon[:num], price: coupon[:cost]/coupon[:num],
   clearance: cart[coupon[:item]][:clearance]}
