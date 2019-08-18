2aa46838f8f442de5050f402248fb37f8ec637c9
Mykle jones <mykle.j91@gmail.com>
2019-08-18 23:22:05 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,10 +1,34 @@
+require "pry"
+
 def consolidate_cart(cart)
-  # code here
+ hash={}
+ cart.each_with_index do |item,index|
+   item.each do |food,info|
+     if hash [food]
+       hash[food][:count]+=1 
+     else
+       hash [food]=info
+       hash [food][:count]=1 
+     end
+    end
+  end
+  hash
 end
 
 def apply_coupons(cart, coupons)
-  # code here
-end
+  
+  hash={}
+  cart.each do |item, index|
+   coupons.each do |coupon|
+  #binding.pry 
+   if coupon[:item]==item
+   hash["#{item} W/COUPON"] = {count: coupon[:num], price: coupon[:cost]/coupon[:num],
+  clearance: cart[coupon[:item]][:clearance]}
+    end 
+   end
+  end
+  hash
+ end
 
 def apply_clearance(cart)
   # code here
