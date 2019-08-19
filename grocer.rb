021475f5a3ad31b68d8b98df9a506413c494dae4
Mykle jones <mykle.j91@gmail.com>
2019-08-19 00:01:10 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -36,7 +36,7 @@ cart[coupon[:item]][:count]-=coupon[:num]
 def apply_clearance(cart)
   cart.keys.each do |item|
 if cart[item][:clearance]
-  cart[item][:price] = (cart[item][:price]*0.80)
+  cart[item][:price] = (cart[item][:price]*0.20)-(cart[item][:price])
    end
   end
   cart
