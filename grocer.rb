cfa30c73418b6f2519be57c98ffe0fac755ff920
Mykle jones <mykle.j91@gmail.com>
2019-08-19 00:42:20 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -44,27 +44,16 @@ if cart[item][:clearance]
  end
  
 
-#def checkout(cart, coupons)
- # consolidate =consolidate_cart(cart)
- # coupons = apply_coupons(consolidate,coupons)
-#  discounts = apply_clearance(discounts)  
-  
- # total=0.0 
- #   total+=discounts[item][:price]*coupons[item][:count]
- # end
-  # if  total > 100.00 
-   #  (total)-(total*0.10)
-  # end
-#end
-
 def checkout(cart, coupons)
-  consol_cart = consolidate_cart(cart)
-  cart_with_coupons_applied = apply_coupons(consol_cart, coupons)
-  cart_with_discounts_applied = apply_clearance(cart_with_coupons_applied)
-
-  total = 0.0
-  cart_with_discounts_applied.keys.each do |item|
-    total += cart_with_discounts_applied[item][:price]*cart_with_discounts_applied[item][:count]
+  consolidate =consolidate_cart(cart)
+  coupons_added = apply_coupons(consolidate,coupons)
+  discounts = apply_clearance(coupons_added)  
+  
+  total=0.0 
+  discounts.keys.each do |item|
+    total+=discounts[item][:price]*coupons[item][:count]
   end
-  total > 100.00 ? (total * 0.90).round : total
+   if  total > 100.00 
+     (total)-(total*0.10)
+   end
 end
