df0bb5b315046391259a5893a0e13ff17312fbdf
Mykle jones <mykle.j91@gmail.com>
2019-08-19 00:55:45 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,58 @@
+require "pry"
+
 def consolidate_cart(cart)
-  # code here
+ hash={}
+ cart.each_with_index do |item,index|
+   item.each do |food,info|
+     if hash [food]
+       hash[food][:count]+=1 
+     else
+       hash [food]=info
+       hash [food][:count]=1 
+     end
+    end
+  end
+  hash
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  coupons.each do |coupon|
+  if cart.keys.include? coupon[:item]
+    if cart[coupon[:item]][:count]>=coupon[:num]
+      name = "#{coupon[:item]} W/COUPON" 
+      if cart[name]
+        cart[name][:count]+=coupon[:num]
+      else
+        cart[name] = {count: coupon[:num], price: coupon[:cost]/coupon[:num],
+  clearance: cart[coupon[:item]][:clearance]}
 end
+cart[coupon[:item]][:count]-=coupon[:num]
+    end 
+   end
+  end
+  cart
+ end
 
 def apply_clearance(cart)
-  # code here
-end
+  cart.keys.each do |item|
+if cart[item][:clearance]
+  
+  cart[item][:price] =(cart[item][:price])- (cart[item][:price]*0.20)
+   end
+  end
+  cart
+ end
+ 
 
 def checkout(cart, coupons)
-  # code here
-end
+  consolidate =consolidate_cart(cart)
+  coupons_added = apply_coupons(consolidate,coupons)
+  discounts = apply_clearance(coupons_added)  
+  
+  total=0.0 
+  discounts.keys.each do |item|
+    total += discounts[item][:price ]*discounts[item][:count]
+  end
+    total > 100.00 ? (total)-(total*0.10): total
+   end
+
